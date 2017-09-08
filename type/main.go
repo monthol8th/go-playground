eb6c6d70ad7efc94592eba685af68acbd924f3a4
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-08 21:08:05 +0700

    playing around type

---
+++ type/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("1+1=",1.0+1.0);
+    fmt.Println("Plankung"+" Eiei");
+    fmt.Println(len("Plankung"));
+    fmt.Println("Plankung"[0]);
+}
