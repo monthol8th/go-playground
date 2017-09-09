c1ec4ce20e21834933a6754a95918edf619cbdab
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-09 17:48:06 +0700

    playing around defer

--- function/main.go
+++ function/main.go
@@ -23,12 +23,32 @@ func generator(multipler int) (func (int) int) {
     }
 }
 
+func incrementgen() (func () int) {
+    i:=0
+    return func () int {
+        ret := i
+        i++
+        return ret
+    }
+}
+
+func end(text string) {
+    fmt.Println(text)
+}
+
 func main() {
+
+    defer end("eiei")
     x := [10]int{1,2,3,4,5,6,7,8,9,10}
     fmt.Println(avg(x[:]))
     spreadit(x[:]...)
     mul1:=generator(2)
     mul2:=generator(4)
 
-    fmt.Println(mul1(10),mul2(10))
+    fmt.Println(mul1(10),mul2(10),mul1(10))
+    inc := incrementgen()
+    inc2 := incrementgen()
+    fmt.Println(inc2())
+    fmt.Println(inc())
+    fmt.Println(inc())
 }
