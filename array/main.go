0d9e28bf68eb851033f3e0b033a6e79b1ff6faec
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-09 15:14:20 +0700

    playing around slice and map

--- array/main.go
+++ array/main.go
@@ -10,4 +10,18 @@ func main() {
         fmt.Println(a[i])
     }
 
+    var slicea []int
+    slicea = a[:]
+
+    for _,x := range slicea {
+        fmt.Println(x)
+    }
+
+    var x map[string]int
+
+    x = make(map[string]int)
+    x["plan"]=1
+    fmt.Println(x["plan"])
+    _,ok := x["plan2"]
+    fmt.Println(ok)
 }
